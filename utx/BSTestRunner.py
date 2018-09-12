493a3cf585ac60d264b1e7c20501feb1a24b1fc3
zhanglei <zhspark@gmail.com>
2018-09-12 14:18:39 +0800

    update

--- utx/BSTestRunner.py
+++ utx/BSTestRunner.py
@@ -42,6 +42,7 @@ import sys
 import unittest
 from xml.sax import saxutils
 
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 __author__ = "Wai Yip Tung && Eason Han"
 __version__ = "0.8.4"
 
