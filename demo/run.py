041d25c9121a186a2a276d1b1f0b3927d558b6f6
zhanglei <zhspark@gmail.com>
2018-09-18 11:27:08 +0800

    add full

--- demo/run.py
+++ demo/run.py
@@ -6,8 +6,8 @@ import logging
 import sys
 
 if __name__ == '__main__':
-    # setting.run_case = {Tag.FULL}  # 运行全部测试用例
-    setting.run_case = {Tag.SMOKE}  # 只运行SMOKE标记的测试用例
+    setting.run_case = {Tag.FULL}  # 运行全部测试用例
+    # setting.run_case = {Tag.SMOKE}  # 只运行SMOKE标记的测试用例
     # setting.run_case = {Tag.SMOKE, Tag.SP}   # 只运行SMOKE和SP标记的测试用例
     setting.check_case_doc = False  # 关闭检测是否编写了测试用例描述
     setting.full_case_name = True
