14d23511ee6d922c894a4eff72236288371dea63
zhanglei <zhspark@gmail.com>
2018-09-18 15:19:42 +0800

    update arguments

--- demo/run.py
+++ demo/run.py
@@ -4,20 +4,21 @@
 from utx import *
 import logging
 import sys
-
+from optparse import OptionParser
 if __name__ == '__main__':
-    setting.run_case = {Tag.FULL}  # 运行全部测试用例
-    # setting.run_case = {Tag.SMOKE}  # 只运行SMOKE标记的测试用例
-    # setting.run_case = {Tag.SMOKE, Tag.SP}   # 只运行SMOKE和SP标记的测试用例
+    parser = OptionParser(usage="usage: %prog [options]",version="%prog 1.0")
+    options,args = parser.parse_args()
+	if len(args) != 4:
+        parser.error("wrong number of arguments")
+    setting.run_case = {args[0]}
     setting.check_case_doc = False  # 关闭检测是否编写了测试用例描述
     setting.full_case_name = True
     setting.max_case_name_len = 80  # 测试报告内，显示用例名字的最大程度
     setting.show_error_traceback = True  # 执行用例的时候，显示报错信息
     setting.sort_case = True  # 是否按照编写顺序，对用例进行排序
-    setting.create_bstest_style_report = False  # 生成bstest风格的报告
-    setting.create_ztest_style_report = True # 生成ztest风格的报告
-
-    log.set_level(logging.DEBUG)  # 设置utx的log级别
+    setting.create_bstest_style_report = args[1]
+    setting.create_ztest_style_report = args[2]
+    log.set_level(logging.DEBUG) = args[3]
 
     runner = TestRunner()
     runner.add_case_dir(r"testcase")
